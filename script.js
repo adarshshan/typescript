cb43bae104186a29d33978350d34191b15777123
Adarsh <adarshshanu3@gmail.com>
2024-01-27 13:58:23 +0530

    executed code

---
+++ script.js
@@ -0,0 +1,15 @@
+//inferTypes (Implicit types)
+// let userName='Adarsh';
+// let toatal=30_0000;
+// toatal='100';
+//Defining type (explicit types)
+var userName = 'Adarsh';
+var subscriber = 30000;
+var subscribed = true;
+var skills = ['javascript', 'node', 'expless', 'mongodb', 'postgresSQL', 'html and css', 'bootstrap', 'typeScript', 'git'];
+var mark = [23, 56, 342, 5];
+var userDetails = {
+    name: 'Adarsh',
+    age: 23,
+    salary: 96000
+};
--- script.ts
+++ script.ts
@@ -1,1 +1,17 @@
-ehl
+//inferTypes (Implicit types)
+// let userName='Adarsh';
+// let toatal=30_0000;
+// toatal='100';
+
+
+//Defining type (explicit types)
+let userName: string = 'Adarsh'
+let subscriber: number = 30_000;
+let subscribed: boolean = true;
+let skills: string[] = ['javascript', 'node', 'expless', 'mongodb', 'postgresSQL', 'html and css', 'bootstrap', 'typeScript', 'git'];
+let mark:number[]=[23,56,342,5];
+let userDetails:{name:string,age:number,salary:number}={
+    name:'Adarsh',
+    age:23,
+    salary:96000
+};
